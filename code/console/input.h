8cdfa617ec558cd7114fc48bffc1511bedba4f79
Sokus <soqs687@gmail.com>
2022-07-30 19:41:30 +0200

    nuke

--- code/console/input.h
+++
@@ -1,8 +0,0 @@
-#ifndef INPUT_H
-#define INPUT_H
-
-#include "console/keycode.h"
-
-KeyCode GetInput();
-
-#endif
--- code/utility/macros.h
+++
@@ -1,7 +0,0 @@
-#include <stddef.h>
-
-#define OFFSETOF(member, type) ((size_t) &((type*)0)->member)
-
-#define CONTAINEROF(ptr, type, member) (type*)((char*)ptr - OFFSETOF(member, type))
-
-#define SWAP(a, b, type) do { type SWAP = x; x = y; y = SWAP; } while (0)
